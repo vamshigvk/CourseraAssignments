2b2ecd8f108415aa86230db7c9bde1b3812df12f
G Vamshi Krishna <vamshikrishnagvk5@gmail.com>
2019-09-18 11:19:52 +0530

    Add files via upload
    https://d3c33hcgiwev3.cloudfront.net/_2a0d228f9a48b3de85eedf022225fac9_ProgAssignment3.pdf?Expires=1568678400&Signature=enSFTl7vJ6cikPNMf7ce7pzuZFgN~yEa9526snScKB2eFnh4l69Xj1GFiFGeHNi4DzWvfx9I7rZmW91DXvae91-RfHB9kHTJvcUZrlr-6cY8592dxCHdpnRg7WoyLesXgdPurmBDnLc96yIc5Bq~xiD7YDW9IPptc0cfMPCPVp8_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A

---
+++ R programming/week4/hospitalAssignment.R
@@ -0,0 +1,32 @@
+#Plot the 30-day mortality rates for heart attack
+plot1<-function(){
+  outcome=read.csv("outcome-of-care-measures.csv",colClasses = "character")
+  head(outcome)
+  outcome[,11]=as.numeric(outcome[,11])
+  hist(outcome[,11])
+  #plot(factor(outcome$State), outcome[,11])
+  
+}
+
+#Finding the best hospital in a state
+best <- function(state, outcome) {
+  outcome.data=read.csv("outcome-of-care-measures.csv",colClasses = "character")
+  
+  if(state %in% outcome.data$State && outcome %in% c("heart attack", "heart failure",  "pneumonia") ){
+    
+    state.filter = outcome.data[outcome.data$State==state,]
+    
+    number=switch(outcome , "heart attack"=as.numeric(11),"heart failure"=17, "pneumonia"=22)
+    
+    state.numeric = state.filter[,number]
+    
+    state.minimum=min(state.numeric,na.rm = FALSE)
+    
+    state.a=state.filter[,number]
+    
+    state.row=state.filter[state.a==state.minimum,]
+    
+    state.best.hospital=state.row["Hospital.Name"]
+  }
+  return(state.best.hospital)
+}
