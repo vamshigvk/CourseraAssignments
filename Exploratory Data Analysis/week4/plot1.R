286b0598954343592e8f42f0f1373b33d06d2626
G Vamshi Krishna <vamshikrishnagvk5@gmail.com>
2019-11-16 14:49:54 +0530

    Add files via upload

--- Exploratory Data Analysis/week4/plot1.R
+++ Exploratory Data Analysis/week4/plot1.R
@@ -1,5 +1,15 @@
-NEI <- readRDS("summarySCC_PM25.rds")
-SCC <- readRDS("Source_Classification_Code.rds")
+FileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+destfile <- "exploratory_data_analysis_week4_project/dataFile.zip"
+
+if(!file.exists(destfile)) {
+  download.file(FileUrl, 
+                destfile = destfile, 
+                method = "curl")
+  unzip(destfile, exdir = "exploratory_data_analysis_week4_project")
+}
+
+NEI <- readRDS("exploratory_data_analysis_week4_project/summarySCC_PM25.rds")
+SCC <- readRDS("exploratory_data_analysis_week4_project/Source_Classification_Code.rds")
 
 totalEmission <- tapply(NEI$Emissions, NEI$year, sum)
 png(filename = 'exploratory_data_analysis_week4_project/plot1.jpg')
--- Exploratory Data Analysis/week4/plot3.R
+++ Exploratory Data Analysis/week4/plot3.R
@@ -1,3 +1,13 @@
+FileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+destfile <- "exploratory_data_analysis_week4_project/dataFile.zip"
+
+if(!file.exists(destfile)) {
+  download.file(FileUrl, 
+                destfile = destfile, 
+                method = "curl")
+  unzip(destfile, exdir = "exploratory_data_analysis_week4_project")
+}
+
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
